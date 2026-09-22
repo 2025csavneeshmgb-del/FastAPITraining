75eb77bc672529695155472086ef278412c1c895
AVNEESH M G <2025cs_avneeshmg_b@nie.ac.in>
2026-09-22 15:19:54 +0530

    add POST and path parameter endpoints, and implement Pydantic model for item creation

--- Day/Day2/Fastapipractice/main.py
+++ Day/Day2/Fastapipractice/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 @app.get("/")
 def home():
@@ -13,3 +13,28 @@ def about():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+#POST request
+@app.post("/create")
+def create_something():
+        return {"message": "Created successfully"}
+
+#path parameters
+@app.get("/student/{usn}")
+def get_student(usn):
+    return {"Result":"Distinction","usn": usn}
+
+#path parameters with type hint
+@app.get("/candidate/{rollno}")
+def get_candidate(rollno:int):
+    return {"Result":"Distinction","rollno": rollno,"type":str(type(rollno))}
+
+#Pydantic model
+class Item(BaseModel):
+    name: str
+    price: float
+    is_stock: bool = True
+
+@app.post("/item")
+def create_item(item: Item):
+    return {"recieved": item, "total_price": item.price*1.8}
